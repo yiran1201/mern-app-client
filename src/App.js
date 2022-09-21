e5562bc366bc2306655efc0c96b74aa71e469d60
yiran1201 <yiranchen1201@gmail.com>
2022-09-21 16:59:32 -0500

    delete problem need to be fixed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
   //add new todo item to database
+  const [itemText, setItemText] = useState('');
+  const [listItems, setListItems] = useState([]);
+  const addItem = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5500/api/item', {
+        item: itemText,
+      });
+      console.log(res);
+      setItemText('');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //Create function to fetch all todo items from database -- we will use useEffect hook
+  useEffect(() => {
+    const getItemsList = async () => {
+      try {
+        const res = await axios.get('http://localhost:5500/api/items');
+        setListItems(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getItemsList();
+  }, []);
+
+  //Delete item when click on delete
+  const deleteItem = async (id) => {
+    try {
+      const res = await axios.delete(`http://localhost:5500/api/item/${id}`);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className='App'>
       <h1>Todo List</h1>
-      <form className="form">
-        <input type='text' placeholder='Add Todo Item' />
+      <form className='form' onSubmit={(e) => addItem(e)}>
+        <input
+          type='text'
+          placeholder='Add Todo Item'
+          onChange={(e) => {
+            setItemText(e.target.value);
+          }}
+          value={itemText}
+        />
         <button type='submit'>Add</button>
       </form>
       <div className='todo-listItems'>
-        <div className='todo-item'>
-          <p className='item-content'>this is the item 1</p>
-          <button className='update-item'>Update</button>
-          <button className='delete-item'>Delete</button>
-        </div>
-        <div className='todo-item'>
-          <p className='item-content'>this is the item 2</p>
-          <button className='update-item'>Update</button>
-          <button className='delete-item'>Delete</button>
-        </div>
-        <div className='todo-item'>
-          <p className='item-content'>this is the item 3</p>
-          <button className='update-item'>Update</button>
-          <button className='delete-item'>Delete</button>
-        </div>
+        {listItems.map((item) => (
+          <div className='todo-item'>
+            <p className='item-content'>{item.item}</p>
+            <button className='update-item'>Update</button>
+            <button
+              className='delete-item'
+              onClick={() => {
+                deleteItem(item._id);
+              }}>
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
