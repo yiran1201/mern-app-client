78a722c6f894299011d8331ed35f17f9bf07200b
Zidian Lyu <zidian.lyu@bytedance.com>
2022-09-21 15:16:25 -0700

    Fix the delete and getItems.

--- src/App.js
+++ src/App.js
@@ -2,43 +2,49 @@ import './App.css';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
+const ORIGIN = 'http://localhost:5500/api';
+
 function App() {
   //add new todo item to database
   const [itemText, setItemText] = useState('');
   const [listItems, setListItems] = useState([]);
-  const addItem = async (e) => {
-    e.preventDefault();
+
+  //Create function to fetch all todo items from database -- we will use useEffect hook
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const getItems = async () => {
     try {
-      const res = await axios.post('http://localhost:5500/api/item', {
-        item: itemText,
-      });
-      console.log(res);
-      setItemText('');
+      const res = await axios.get(`${ORIGIN}/items`);
+      setListItems(res.data);
     } catch (err) {
       console.log(err);
     }
   };
 
-  //Create function to fetch all todo items from database -- we will use useEffect hook
-  useEffect(() => {
-    const getItemsList = async () => {
-      try {
-        const res = await axios.get('http://localhost:5500/api/items');
-        setListItems(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getItemsList();
-  }, []);
+  const addItem = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${ORIGIN}/item`, {item: itemText});
+      console.log(res);
+      setItemText('');
+    } catch (err) {
+      console.log(err);
+    } finally {
+      getItems();
+    }
+  };
 
   //Delete item when click on delete
   const deleteItem = async (id) => {
     try {
-      const res = await axios.delete(`http://localhost:5500/api/item/${id}`);
+      const res = await axios.delete(`${ORIGIN}/item/${id}`);
       console.log(res.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      getItems();
     }
   };
 
@@ -57,15 +63,13 @@ function App() {
         <button type='submit'>Add</button>
       </form>
       <div className='todo-listItems'>
-        {listItems.map((item) => (
-          <div className='todo-item'>
+        {listItems.map((item, i) => (
+          <div className='todo-item' key={i}>
             <p className='item-content'>{item.item}</p>
             <button className='update-item'>Update</button>
             <button
               className='delete-item'
-              onClick={() => {
-                deleteItem(item._id);
-              }}>
+              onClick={() => deleteItem(item._id)}>
               Delete
             </button>
           </div>
